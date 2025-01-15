f4762a825028c47776d36fa900285257e5fd1bf7
nf3lix <felix01.schneider@gmail.com>
2025-01-15 11:56:51 +0100

    feat: Ensure Redis is available

--- src/main/java/de/dhbw/JedisConfig.java
+++ src/main/java/de/dhbw/JedisConfig.java
@@ -1,14 +1,20 @@
 package de.dhbw;
 
 import io.rebloom.client.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class JedisConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(JedisConfig.class);
 
     @Bean
     public JedisPool jedisPool(@Value("${redis.host}") String redisHost,
@@ -28,6 +34,21 @@ public class JedisConfig {
     @Bean
     public Client bloomFilterClient(JedisPool jedisPool) {
         return new Client(jedisPool);
+    }
+
+    @Bean
+    public ApplicationRunner jedisPoolChecker(JedisPool jedisPool) {
+        return args -> {
+            try (Jedis jedis = jedisPool.getResource()) {
+                if (jedis.ping().equalsIgnoreCase("PONG")) {
+                    logger.info("Redis available");
+                } else {
+                    throw new RuntimeException("Failed to connect to Redis.");
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to connect to Redis.", e);
+            }
+        };
     }
 
 }
